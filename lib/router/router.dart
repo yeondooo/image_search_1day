8696d49affedddc5cfa5ebb08512497f3ef19786
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-03 01:13:07 +0900

    feat: router extra 값 추가

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_search_1day/data/model/photo.dart';
 import 'package:image_search_1day/ui/detail/detail_screen.dart';
 import 'package:image_search_1day/ui/main/main_screen.dart';
 
@@ -14,7 +15,8 @@ final GoRouter router = GoRouter(
         GoRoute(
           path: 'detail',
           builder: (BuildContext context, GoRouterState state) {
-            return const DetailScreen();
+            final photo = state.extra as Photo;
+            return DetailScreen(photo: photo);
           },
         ),
       ],
