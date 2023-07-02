bdcc55ab662b3fd307048a5d2c27d9dd18057935
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-03 01:14:36 +0900

    feat: main screen 구현

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,13 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_search_1day/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final imageViewModel = context.watch<MainViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('이미지 검색 앱'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                labelText: '검색어를 입력하세요.',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    imageViewModel.fetchImages(controller.text);
+                  },
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                ),
+                itemCount: imageViewModel.photos.length,
+                itemBuilder: (context, index) {
+                  final photo = imageViewModel.photos[index];
+                  return Hero(
+                    tag: photo.id,
+                    child: GestureDetector(
+                      onTap: () {
+                        context.push('/detail', extra: photo);
+                      },
+                      child: Image.network(
+                        photo.url,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                }),
+          ),
+        ],
       ),
     );
   }
