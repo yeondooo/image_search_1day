bed7a4c81d78a26855d403618748fdb50b14ff05
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-03 01:13:51 +0900

    feat: photo mapper 구현

---
+++ lib/data/mapper/photo_mapper.dart
@@ -0,0 +1,13 @@
+import 'package:image_search_1day/data/dto/pixabay_result_dto.dart';
+import 'package:image_search_1day/data/model/photo.dart';
+
+extension ToPhoto on Hits {
+  Photo toPhoto() {
+    return Photo(
+      id: id ?? 0,
+      url: webformatURL ?? '',
+      tags: tags ?? '',
+      views: views ?? 0,
+    );
+  }
+}
